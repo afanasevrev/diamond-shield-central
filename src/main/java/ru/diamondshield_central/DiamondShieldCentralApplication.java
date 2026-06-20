5f2051eb52fb3aa45e1fc874d5299a69251446bc
revoly afanasev <rewoly@mail.ru>
2026-06-20 19:29:12 +0900

    first commit

---
+++ src/main/java/ru/diamondshield_central/DiamondShieldCentralApplication.java
@@ -0,0 +1,13 @@
+package ru.diamondshield_central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondShieldCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondShieldCentralApplication.class, args);
+	}
+
+}
