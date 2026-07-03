37712839cd1934a940837bca77a7c0cb8422b3d8
revoly afanasev <rewoly@mail.ru>
2026-07-03 23:27:50 +0900

    Запуск состоялся

--- src/main/java/ru/diamondshield_central/config/SecurityConfig.java
+++ src/main/java/ru/diamondshield_central/config/SecurityConfig.java
@@ -51,11 +51,10 @@ public class SecurityConfig {
             throws Exception {
         return configuration.getAuthenticationManager();
     }
-    /**
+/**
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
     }
-    **/
-    // Пока убираем, потому то он есть в PassworConfig
+**/
 }
